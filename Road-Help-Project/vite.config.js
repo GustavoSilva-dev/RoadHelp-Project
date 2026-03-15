77f9d91b1adcba70de06a852b0cb684c70f9d6f1
Gustavo <gustavosilvaxx25@gmail.com>
2026-03-14 23:21:18 -0300

    Implementação do PWA

--- Road-Help-Project/vite.config.js
+++ Road-Help-Project/vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 import fs from "fs"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({ registerType: 'autoUpdate' })
+  ],
   server: {
     open: true
 }})
